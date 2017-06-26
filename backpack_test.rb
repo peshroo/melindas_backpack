00ad5069748b948d7e69db6e6c212939526f5063
Roopesh <rrattan.89@gmail.com>
2017-06-26 15:04:09 -0400

    code smell 0 - insufficient tests

--- backpack_test.rb
+++ backpack_test.rb
@@ -45,7 +45,19 @@ class BackpackTest < MiniTest::Test
 
   ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
   # is something missing here?
+  def test_that_has_shoes_mondays_thursdays
+    ['monday', 'thursday'].each do |day|
+      backpack = build.(:backpack, day_of_week: day)
+      assert(backpack.items.include?('gym shoes'))
+    end
+  end
 
+  def test_that_doesnt_have_shoes_except_monday_thursday
+    ['tuesday', 'wednesday', 'friday', 'saturday', 'sunday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      refute(backpack.items.include?('gym shoes'))
+    end
+  end
 
   ##### TESTS ABOUT PRINTING THE LIST #####
   def test_that_backpack_prints_correctly
