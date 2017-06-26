bd2d58f8218e1b4a6620e797c36e17f86389ec27
Roopesh <rrattan.89@gmail.com>
2017-06-26 15:37:32 -0400

    code smell 6 - unnecessary comments

--- backpack.rb
+++ backpack.rb
@@ -1,6 +1,6 @@
 class Backpack
   def initialize(attributes)
-    @attributes = attributes # a hash containing day_of_week and weather keys
+    @attributes = attributes
     @items = []
     prepare
   end
@@ -10,11 +10,9 @@ class Backpack
   end
 
   def prepare
-    # set up local variables used by rest of prepare method
     weather = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
 
-    # Ensure appropriate clothing is added to backpack
     if weather == 'rainy'
       atleast_pants_shirt
       @items << 'umbrella'
@@ -29,20 +27,17 @@ class Backpack
     @items << 'pants'
     @items << 'shirt'
   end
-    # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Mondays and Thursdays. Wait a sec...
+
     if day_of_week == 'monday' || day_of_week == 'thursday'
       #add gym shoes to items
       @items << 'gym shoes'
     end
 
-    # Bring a packed lunch on all weekdays
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     end
   end
 
-  # Prints a summary packing list for Melinda's backpack
   def packing_list
     output = []
     output << "Melinda, here's your packing list!"
